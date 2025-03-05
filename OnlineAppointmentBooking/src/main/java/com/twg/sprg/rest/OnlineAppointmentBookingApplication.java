c1c3bd110c10cc3316b4710f9ec22041b7a4fe67
NimmanapalliHarika <nimmanapalliharika@gmail.com>
2025-03-05 23:33:32 +0530

    first commit

---
+++ OnlineAppointmentBooking/src/main/java/com/twg/sprg/rest/OnlineAppointmentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.twg.sprg.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAppointmentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAppointmentBookingApplication.class, args);
+	}
+
+}
